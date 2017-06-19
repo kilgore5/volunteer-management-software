73259ef98b00fb51edf1788507224ae734e81fe7
Jarvis Johnson <jarviscjohnson@gmail.com>
2017-06-18 21:27:41 -0400

    clean up header and fix logic to redirect user from homepage if logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,14 @@ class PagesController < ApplicationController
   # before_action :redirect_to_account, only: [:home]
   
   def home
-    @applied = user_has_applied?
+    @applied = user_has_applied
     if @applied
       redirect_to edit_user_path(current_user)
     end    
     @events = Event.includes(:client_owner, :event_days, :jobs).order('start_time ASC')
   end
 
-  def user_has_applied?
+  def user_has_applied
     if user_signed_in?
       if @current_event.applications.find_by(user_id: current_user.id).nil?
         return false
@@ -20,6 +20,7 @@ class PagesController < ApplicationController
     else
       return false
     end
+    return true
   end
 
 end
