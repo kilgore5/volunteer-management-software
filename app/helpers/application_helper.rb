ca9063295bca8eeebd4c7eb02b2733a2fffdae0e
Jarvis Johnson <jarviscjohnson@gmail.com>
2017-06-27 23:34:06 -0400

    bringing styling into line with main strawberry site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   # Creates flash messages
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade show") do 
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} show") do 
               concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
               concat message 
             end)
