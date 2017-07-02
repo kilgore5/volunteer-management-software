0b0d3f1623950d8ccfa67b2ca0d09194fa7c7b95
Jarvis Johnson <jarviscjohnson@gmail.com>
2017-07-02 12:30:41 -0400

    aws config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,20 @@ Rails.application.configure do
      :authentication => :plain
   }
 
+  # config.paperclip_defaults = {
+  #   storage: :s3,
+  #   s3_credentials: {
+  #     bucket: ENV.fetch('S3_BUCKET_NAME'),
+  #     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+  #     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+  #     s3_region: ENV.fetch('AWS_REGION'),
+  #   }
+  # }
+
   config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-    }
+    :storage => :s3,
+    # :s3_host_name => 'REMOVE_THIS_LINE_IF_UNNECESSARY',
+    :bucket => ENV["S3_BUCKET_NAME"]
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
