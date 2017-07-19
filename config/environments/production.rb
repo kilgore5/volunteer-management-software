83a435dcef50bc0555e4bb63aab49e73a8b37de8
Jarvis Johnson <jarviscjohnson@gmail.com>
2017-07-19 08:23:56 -0400

    email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-     :address => "smtp.gmail.com",
-     :port => 587,
+     :address => "webcloud1.au.syrahost.com",
+     :port => 465,
      :user_name => ENV["gmail_username"],
      :password => ENV["gmail_password"],
      :authentication => :plain
